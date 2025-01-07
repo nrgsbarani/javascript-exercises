b622ef0668aa1f7a38bed80f94a8100c5ddb5844
Amir Dadipoor <amirdadipoor>
2025-01-07 23:34:04 +0330

    resolve fib problem

--- intermediate/fibonacci.js
+++ intermediate/fibonacci.js
@@ -11,5 +11,27 @@
  */
 
 // Write your solution here
+function fibonacciRecursive(number) {
+    if(number === 0) { return 0}
+    else if (number === 1) { return 1 }
+    else {
+        return fibonacciRecursive(number-1) + fibonacciRecursive(number-2);
+    }
+}
+function fibonacci(number) {
+
+    if (number === 0) { return []; }
+    else if (number === 1) {return [0]}
+    else if (number === 2) {return [0,1]}
+    else {
+        let arr = new Array(number);
+        arr[0] = 0
+        arr[1] = 1;
+        for (let i = 2; i < arr.length; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2] ;
+        }
+        return arr;
+    }
+}
 
 module.exports = fibonacci;
