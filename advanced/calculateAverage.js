133486c4e78104e12d4a237f7038237b1856b83e
amir dadipoor <amirdadipoor@gmail.com>
2025-01-08 07:38:21 +0330

    resolve average

--- advanced/calculateAverage.js
+++ advanced/calculateAverage.js
@@ -9,5 +9,5 @@
  * calculateAverage([1, 2, 3, 4]) should return 2.5.
  * calculateAverage([10, 20, 30]) should return 20.
  */
-
+calculateAverage = (arr) => (arr.reduce((a, b) => a + b , 0)) / arr.length;
 module.exports = calculateAverage;
