0379b54207fa962612b2b45cbf887d06067f4a6e
amir dadipoor <amirdadipoor@gmail.com>
2025-01-17 17:18:22 +0330

    other

--- advanced/numberToWords.js
+++ advanced/numberToWords.js
@@ -12,4 +12,34 @@
 
 // Write your solution here
 
+function numberToWords(input){
+    const Numbers = new Map([
+        [0 , "zero"],
+        [1 , "one"],
+        [2 , "two"],
+        [3 , "three"],
+        [4 , "four"],
+        [5 , "five"],
+        [6 ,"six"],
+        [7 ,"seven"],
+        [8 ,"eight"],
+        [9 ,"nine"],
+        [10 , "ten"],
+        [11 , "eleven"],
+        [12, "twelve"],
+        [13, "thirteen"],
+        [14, "fourteen"],
+        [15, "fifteen"],
+        [16, "sixteen"],
+        [17, "seventeen"],
+        [18, "eighteen"],
+        [19, "eleventeen"],
+    ])
+    let array = new String( input + "").split("").map(Number);
+    for
+
+}
+
 module.exports = numberToWords;
+
+
