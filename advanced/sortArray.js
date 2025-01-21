08d6c786b07ba4d8eb1ccd4bf7832ae353d3b418
Ata Jarrah <jarrah66ata@gmail.com>
2025-01-21 17:08:41 +0330

    Done exercises v1.0.2

--- advanced/sortArray.js
+++ advanced/sortArray.js
@@ -9,4 +9,22 @@
  * sortArray([3, 1, 4, 1, 5, 9]) should return [1, 1, 3, 4, 5, 9].
  */
 
+// Bubble sort
+function sortArray(list_of_numbers){
+    len = list_of_numbers.length;
+    for (i = 0 ; i < len ; i++){
+        for(j = 0; j <= len -1; j++){
+            if (list_of_numbers[j] > list_of_numbers[j+1]){
+                var variable = list_of_numbers[j]
+                list_of_numbers[j] = list_of_numbers[j+1]
+                list_of_numbers[j+1] = variable
+            } else {
+                continue
+            }
+        }
+    }
+    console.log(len)
+    return list_of_numbers
+}
+
 module.exports = sortArray;
