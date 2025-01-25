6ce21f82525e99ed6171a05527b23c89c5474441
Narges Barani <narges18x.b@gmail.com>
2025-01-25 07:34:53 -0800

    Solved exercises calculator and generate combinations

--- advanced/calculator.js
+++ advanced/calculator.js
@@ -14,5 +14,14 @@
  */
 
 // Write your solution here
+add = (a , b) => { return a + b; }
+subtract = (a , b) => { return a - b; }
+multiply = (a , b) => { return a * b; }
+divide = (a , b) => {
+    if (b === 0) {
+        return NaN;
+    }
+    return a / b;
+}
 
 module.exports = { add, subtract, multiply, divide };
--- advanced/generateCombinations.js
+++ advanced/generateCombinations.js
@@ -10,5 +10,27 @@
  * generateCombinations([1, 2, 3]) should return
  * [ [1], [2], [3], [1, 2], [1, 3], [2, 3], [1, 2, 3] ].
  */
+generateCombinations = (arr) => {
+    let result = [];
+    
+    // Helper function to create Combination
+    function backtrack(start, current) {
+        // add current combination 
+        if (current.length > 0) {
+            result.push([...current]);  // Add current combination to result
+        }
+
+        // add new element to combination
+        for (let i = start; i < arr.length; i++) {
+            current.push(arr[i]);          // add new element to current list (combination list)
+            backtrack(i + 1, current);     // callback function
+            current.pop();                 // remove last element
+        }
+    }
+
+    // callback function from the beginning
+    backtrack(0, []);
+    return result.sort((a, b) => a.length - b.length || a[0] - b[0]);
+}
 
 module.exports = generateCombinations;
