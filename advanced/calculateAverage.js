134324f45fa58c5d0063289f204c5db849f08dfc
mohammadkako <mehdikako85@gmail.com>
2025-01-31 11:15:21 -0800

    average was done

--- advanced/calculateAverage.js
+++ advanced/calculateAverage.js
@@ -9,5 +9,14 @@
  * calculateAverage([1, 2, 3, 4]) should return 2.5.
  * calculateAverage([10, 20, 30]) should return 20.
  */
-
+function calculateAverage(x){
+    if(x.length === 0){
+        return NaN
+    }else{
+        let y = x.reduce(function(total,value){
+            return total + value
+        })
+        return y / x.length
+    }
+}
 module.exports = calculateAverage;
