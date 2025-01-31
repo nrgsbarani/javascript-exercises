a5f71435c32dd153e012a22a8922615dc21f4176
mohammadkako <mehdikako85@gmail.com>
2025-01-31 11:13:47 -0800

    calculator was made

--- advanced/calculator.js
+++ advanced/calculator.js
@@ -14,5 +14,21 @@
  */
 
 // Write your solution here
+function add (a,b){
+    return a+b
+}
+function subtract (a,b){
+    return a-b
+}
+function multiply (a,b){
+    return a*b
+}
+function divide (a,b){
+        if (b==0){
+            return NaN
+        }else{
+            return a/b
+        }
 
+}
 module.exports = { add, subtract, multiply, divide };
