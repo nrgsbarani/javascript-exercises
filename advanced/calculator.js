555f41263d4dd84cceefb299696adc39611fc654
Sareh <maturisareh@gmail.com>
2025-02-01 10:50:40 +0330

    solve calculator

--- advanced/calculator.js
+++ advanced/calculator.js
@@ -16,3 +16,25 @@
 // Write your solution here
 
 module.exports = { add, subtract, multiply, divide };
+
+function add(a,b){
+    return a+b;
+}
+function subtract(a,b){
+    return a-b;
+}
+function multiply(a,b){
+    return a*b;
+}
+function divide(a,b){
+    if ( b === 0) {
+        return NaN;
+    } else {
+        return a/b;
+    }
+}
+
+console.log(add(2, 3));
+console.log(subtract(5, 2)); 
+console.log(multiply(2, 4)); 
+console.log(divide(10, 2));
