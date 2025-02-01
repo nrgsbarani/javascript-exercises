dcbe559d1d4213f6d6f9663db3760f56a5ab4a40
Sareh <maturisareh@gmail.com>
2025-02-01 11:38:23 +0330

    solve find Duplicates

--- advanced/findDuplicates.js
+++ advanced/findDuplicates.js
@@ -13,3 +13,22 @@
 // Write your solution here
 
 module.exports = findDuplicates;
+
+function findDuplicates(array) {
+    const duplicates = [];
+    const seen = new Set();
+    for (let i = 0; i < array.length; i++) {
+        if (seen.has(array[i])) {
+            if (!duplicates.includes(array[i])) {
+                duplicates.push(array[i]);
+            }
+        } else {
+            seen.add(array[i]);
+        }
+    }
+    return duplicates;
+}
+
+console.log(findDuplicates([1, 2, 2, 3, 4, 4]));
+console.log(findDuplicates([1, 1, 1, 2, 3]));
+console.log(findDuplicates([1, 2, 3]));
