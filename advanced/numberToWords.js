3af5e89292d369f45a203d2b5d2e7ec0e04a364d
mohammadkako <mehdikako85@gmail.com>
2025-02-02 10:07:11 -0800

    number convertor was added

--- advanced/numberToWords.js
+++ advanced/numberToWords.js
@@ -12,4 +12,53 @@
 
 // Write your solution here
 
+function numberToWords(number) {
+    if (number === 0) {
+      return "zero";
+    }
+  
+    const units = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    const teens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+    const tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+    const thousands = ["", "thousand", "million", "billion"];
+  
+    function convertLessThanThousand(num) {
+      if (num === 0) {
+        return "";
+      } else if (num < 10) {
+        return units[num];
+      } else if (num < 20) {
+        return teens[num - 10];
+      } else if(num < 99) {
+        const ten = Math.floor(num / 10);
+        const unit = num % 10;
+        return tens[ten] + " " + units[unit];
+      }else {
+        const hundreds = Math.floor(num / 100);
+        const un = num % 100;
+        const unit = String(un).split("").map(Number)
+        if(unit[0] === 1){
+            return units[hundreds] + " hundred " + teens[unit[1]]
+        }else{
+            return units[hundreds] + " hundred " + tens[unit[0]] + " " + units[unit[1]];
+        }
+      }
+    }
+  
+    let words = "";
+    let i = 0;
+  
+    while (number > 0) {
+      const chunk = number % 1000;
+      if (chunk !== 0) {
+        words = convertLessThanThousand(chunk) + " " + thousands[i] + " " + words;
+      }
+      number = Math.floor(number / 1000);
+      i++;
+    }
+  
+    return words.trim();
+  }
+
+
 module.exports = numberToWords;
