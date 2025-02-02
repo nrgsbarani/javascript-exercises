6e840346da5d150dd066e3f54dfed346141b38c2
mohammadkako <mehdikako85@gmail.com>
2025-02-02 10:04:55 -0800

    sum of digits was readded

--- advanced/sumOfDigits.js
+++ advanced/sumOfDigits.js
@@ -12,7 +12,7 @@
 
 function sumOfDigits(x){
         let y = x+""
-        let numbers = String(y).split("").map(Number)
+        let numbers = String(y).split("").map(Number)//gptاین لاین با 
     
         let sum = 0
         for (let i = 0; i < numbers.length; i++) {
