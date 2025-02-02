04ae3a732c7fd85c6efef4cca4d3463db0adc082
mohammadkako <mehdikako85@gmail.com>
2025-02-02 10:09:20 -0800

    generator was added

--- advanced/generateCombinations.js
+++ advanced/generateCombinations.js
@@ -11,4 +11,32 @@
  * [ [1], [2], [3], [1, 2], [1, 3], [2, 3], [1, 2, 3] ].
  */
 
+function generateCombinations(array){
+    
+    if(array.length === 0){
+        return array
+    }else if(array.length === 1){
+        return [array]
+    }else{
+        let result = []
+
+        
+        for(let i=0 ; i < array.length ; i++){
+            let x = Array.of(array[i])
+            result.push(x)
+        } 
+        
+        for (let i = 0; i <= array.length - 2; i++) {
+        for (let j = i + 1; j < array.length; j++) {//gptاین لاین با 
+        result.push([array[i], array[j]]);
+        }
+        }//gptاین لاین با 
+
+        result.push(array)
+
+        
+
+        return result
+    }
+}
 module.exports = generateCombinations;
