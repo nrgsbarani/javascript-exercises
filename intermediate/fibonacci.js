5358605d033d32c90b9c1b0435d3b765e6e3842e
Sareh <maturisareh@gmail.com>
2025-02-03 11:27:16 +0330

    solve fibonacci

--- intermediate/fibonacci.js
+++ intermediate/fibonacci.js
@@ -13,3 +13,21 @@
 // Write your solution here
 
 module.exports = fibonacci;
+
+function fibonacci(n) {
+    let fib = [];
+    if ( n === 1) {
+        return [0];
+
+    } else if ( n === 2 ) {
+        return [0,1];
+    } else {
+        fib = [0, 1];
+        for (let i = 2; i < n; i++) {
+            fib.push(fib[i-1] + fib[i-2]);
+        }
+    }
+    return fib;
+    }
+
+    console.log(fibonacci(5));
