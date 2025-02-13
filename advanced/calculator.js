8cb47c6dea784f3ebd22d110d4c4c670f444b268
Narges Barani <narges18x.b@gmail.com>
2025-02-13 09:38:57 -0800

    Refactor Advanced practice

--- advanced/calculator.js
+++ advanced/calculator.js
@@ -14,14 +14,9 @@
  */
 
 // Write your solution here
-add = (a , b) => { return a + b; }
-subtract = (a , b) => { return a - b; }
-multiply = (a , b) => { return a * b; }
-divide = (a , b) => {
-    if (b === 0) {
-        return NaN;
-    }
-    return a / b;
-}
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => (b === 0 ? NaN : a / b);
 
 module.exports = { add, subtract, multiply, divide };
