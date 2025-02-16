e5b9bb64ddb0e6cf198bafb28ca9f9f687930237
mohammadkako <mehdikako85@gmail.com>
2025-02-16 10:18:47 -0800

    generator was refactored

--- advanced/generateCombinations.js
+++ advanced/generateCombinations.js
@@ -11,30 +11,27 @@
  * [ [1], [2], [3], [1, 2], [1, 3], [2, 3], [1, 2, 3] ].
  */
 
-function generateCombinations(array){
+generateCombinations = (array) => array.length === 0 ? array : order(array)
     
-    if(array.length === 0){
-        return array
-    }else if(array.length === 1){
+
+function order(array){
+    if(array.length === 1){
         return [array]
     }else{
         let result = []
 
-        
+
         for(let i=0 ; i < array.length ; i++){
-            let x = Array.of(array[i])
-            result.push(x)
+            result.push(Array.of(array[i]))
         } 
         
         for (let i = 0; i <= array.length - 2; i++) {
-        for (let j = i + 1; j < array.length; j++) {//gptاین لاین با 
+            for (let j = i + 1; j < array.length; j++) {//gptاین لاین با 
         result.push([array[i], array[j]]);
         }
         }
 
         result.push(array)
-
-        
 
         return result
     }
