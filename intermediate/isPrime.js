bedd4b853aa6864ba245013634f982c62f5c536e
shaghayeghbeyki78 <shaghayeghbeyki4@gmail.com>
2025-02-23 21:19:44 +0330

    I think I modified a program

--- intermediate/isPrime.js
+++ intermediate/isPrime.js
@@ -11,12 +11,5 @@
  */
 
 // Write your solution here
-const isPrime = n => {
-    if (n < 2 || (n % 2 === 0 && n !== 2)) return false;
-    for (let i = 3; i <= Math.sqrt(n); i += 2) {
-        if (n % i === 0) return false;
-    }
-    return true;
-};
-
+const isPrime = n => n > 1 && [...Array(Math.floor(Math.sqrt(n) - 1)).keys()].every(i => n % (i + 2) !== 0);
 module.exports = isPrime;
